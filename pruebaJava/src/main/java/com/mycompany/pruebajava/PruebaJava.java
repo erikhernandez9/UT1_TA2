f743028c5adc31c724f6cb06313db90352c51c22
erikhernandez9 <erik.hernandez@correo.ucu.edu.uy>
2023-03-20 10:28:18 -0300

    first commit

---
+++ pruebaJava/src/main/java/com/mycompany/pruebajava/PruebaJava.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.pruebajava;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Erik Hernandez
+ */
+public class PruebaJava {
+
+    public static void main(String[] args) {
+        METODO();
+        System.out.println("Hello World!");
+    }
+    
+    
+    // Los nombres de las constantes deben ir siempre en mayúscula
+    int constante = 1;
+    
+    //String 1a = "asd"; Error: ava.lang.RuntimeException: Uncompilable code - <identifier> expected
+    
+    public static void METODO(){
+        // El nombre de los métodos deben comenzar con minúsculas, en caso de que haya más de una palabra, debe comenzar
+        // con mayúscula
+        // Los nombrea de las variables locales deben empezar con minúscula, y en caso de tener más de una palabra, usar
+        // cammelcase
+        String _nombre = "Hola";
+        ArrayList LISTA = new ArrayList();
+        String new = "hola";
+        
+    }
+
+}

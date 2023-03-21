f7ff4cf6854a3b59b14042577da8066145e139ed
Locatelli-Flor <floriana.locatelli@correo.ucu.edu.uy>
2023-03-20 22:15:08 -0300

    Arreglar parte de nombre de variables

--- pruebaJava/src/main/java/com/mycompany/pruebajava/PruebaJava.java
+++ pruebaJava/src/main/java/com/mycompany/pruebajava/PruebaJava.java
@@ -1,37 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.pruebajava;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Erik Hernandez
- */
 public class PruebaJava {
 
+    // Casos de nombres de variables o métodos que no compilan y cuáles errores dan:
+    
+    //Caso 1: variable estática con una palabra reservada como identificador
+    //static int abstract = 5; Error: java.lang.RuntimeException: Uncompilable code - <identifier> expected
+    
     public static void main(String[] args) {
+        
+        // Caso 2: variable de instancia con una palabra reservada como identificador:
+        // int float = 0; Error: Exception in thread "main" java.lang.RuntimeException: Uncompilable code - not a statement
         METODO();
-        System.out.println("Hello World!");
+        metodoPrueba();
+        
     }
     
+    public static void metodoPrueba(){
+        // Caso 3: variable local con una palabra reservada como identificador.
+        // String new = "hola"; Error Exception in thread "main" java.lang.RuntimeException: Uncompilable code - not a statement
+    }
     
-    // Los nombres de las constantes deben ir siempre en mayúscula
-    int constante = 1;
+    // Caso 4: variable de parámetro con una palabra reservada como identificador
+    // public static void metodoPrueba2(int abstract){
     
-    //String 1a = "asd"; Error: ava.lang.RuntimeException: Uncompilable code - <identifier> expected
+    // } Error: java.lang.ClassFormatError: Absent Code attribute in method that is not native or abstract in class file com/mycompany/pruebajava/PruebaJava 
     
-    public static void METODO(){
-        // El nombre de los métodos deben comenzar con minúsculas, en caso de que haya más de una palabra, debe comenzar
-        // con mayúscula
-        // Los nombrea de las variables locales deben empezar con minúscula, y en caso de tener más de una palabra, usar
-        // cammelcase
-        String _nombre = "Hola";
-        ArrayList LISTA = new ArrayList();
-        String new = "hola";
+    // Aclaración: estos errores también ocurren cuando el identificador de una variable (de cualquier tipo) o método comienza con un símbolo o número.
+    
+    
+    // Casos de nombres de variables o métodos que compilan, pero son malas prácticas:
+    
+    // Caso 1: Los nombres de las constantes (variables que no cambian de valor) deben ir siempre en mayúscula, y en caso de tener más de una palabra,
+    // separar con guiones bajos.
+    int constanteNumerica = 1; // Mala práctica, el nombre debería ser "CONSTANTE_NUMERICA"
+    
+    // Caso 2: El nombre de los métodos deben comenzar con minúsculas, y en caso de que haya más de una palabra, debe comenzar con mayúscula.
+    public static void METODO(){ // Mala práctica, el nombre debería ser "metodo".
+        
+        // Caso 3: Los nombres de las variables locales deben empezar con minúscula, y en caso de tener más de una palabra, usar Cammelcase
+        ArrayList LISTA_NUMEROS = new ArrayList(); // Mala práctica, el nombre debería ser "listaNumeros"
+   
+    }
+    
+    // Caso 4: El nombre de las clases o de las interfaces debe comenzar con mayúscula, y en caso de tener más de una palabra, usar Pascalcase
+    public class clase_prueba // Mala práctica, el nombre debería ser "ClasePrueba"
+    {
         
     }
 
